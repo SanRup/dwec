8d4e54c34d9097547f7d63baed8e855eec19c6c3
san <sandra@prueba.org>
2024-01-10 10:45:43 +0100

    10/01 buscaminas

--- Prácticas/300Buscaminas/iniciarBuscaminas.js
+++ Prácticas/300Buscaminas/iniciarBuscaminas.js
@@ -1,3 +1,25 @@
+        window.addEventListener("load", inicio);
+
+        function inicio() {
+            let divDif = document.getElementById("dificultad");
+            let boton = divDif.firstChild;
+                for (let i = 0; i < divDif.children.length; i++) {
+                    boton.addEventListener("click", saludo);
+                    boton = boton.nextElementSibling; 
+                }
+        }
+
+        function saludo(e) {
+            
+            let evento = e.target;
+            alert("hola" + evento.textContent);
+            console.log(evento.textContent);
+        }
+
+        function dibujarTableroHTML() {
+            
+        }
+        
         function generarTableroJS(){
             let 
         };

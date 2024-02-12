12539a208c517073228355f0ee015ae6b5f575fc
san <sandra@prueba.org>
2024-02-12 12:20:28 +0100

    repaso

--- 07_ImportExport/01Ejemplo/modulos.js
+++ 07_ImportExport/01Ejemplo/modulos.js
@@ -2,3 +2,6 @@ import { cadenaMayuscula } from "./funciones.js";
 
 const saludo = cadenaMayuscula("hola mundo");
 console.log(saludo);
+
+import { NUMEROE, NUMEROCERO, NUMEROPI } from "./funciones.js";
+console.log(NUMEROE, NUMEROCERO, NUMEROPI);
